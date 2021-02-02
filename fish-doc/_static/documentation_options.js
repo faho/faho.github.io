370510092bfadbc219599e4ee914d190b309a677
Fabian Homborg <FHomborg@gmail.com>
2021-02-02 08:30:19 +0100

    Update docs

--- fish-doc/_static/documentation_options.js
+++ fish-doc/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.1.2-1985-g261e13e0c',
+    VERSION: '3.1.2-2003-g2faf814da',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
